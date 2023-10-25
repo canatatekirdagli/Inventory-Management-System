4a130bfdfb3b34c697a8df7422e963d90df9349e
Can Ata Tekirdağlı <canatatekirdagli30@gmail.com>
2023-10-25 16:17:02 +0300

    Update main.py
    1. "Parsing and adding shopList to the database."
    2. "Fixed the incorrect calculation error in total sales calculation."
    3. "Added printing of Company Name and Value from shopList in addition to sorted.txt, totalSales.txt, and unchanged_products.txt files."

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cProfile
 import re
 import json
 import sqlite3
@@ -25,13 +26,30 @@ def process_and_parse_data(input_file):
                 raise e
 
     return parsed_data
-
 def write_parsed_data_to_file(data):
     with open("txt/parsedData.txt", 'w', encoding='utf-8') as f:
         for entry in data:
             date_time, seller_id, qty, sku = entry
             f.write(f"Date: {date_time}, seller_id: {seller_id}, qty: {qty}, sku: {sku}\n")
+def add_data_to_database():
+    with sqlite3.connect('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS company
+            (company_name TEXT, sku TEXT, value TEXT)
+        ''')
 
+        already_added_skus = set()
+        with open("txt/shopList.txt", 'r', encoding='utf-8') as file:
+            for line in file:
+                title, sku, value = line.strip().split('\t')
+
+                if sku in already_added_skus:
+                    continue
+
+                cursor.execute('INSERT INTO company (company_name, sku, value) VALUES (?, ?, ?)', (title, sku, value))
+
+                already_added_skus.add(sku)
 def write_to_database(data):
     with sqlite3.connect('data.db') as conn:
         cursor = conn.cursor()
@@ -43,92 +61,116 @@ def write_to_database(data):
 
 
 def generate_stock_changes():
-    sku_cache = {}
-
     with sqlite3.connect('data.db') as conn:
         cursor = conn.cursor()
+        cursor2 = conn.cursor()
 
-        cursor.execute('SELECT sku, qty FROM products ORDER BY sku, created_at')
-        rows = cursor.fetchall()
+        cursor.execute('CREATE INDEX IF NOT EXISTS products_sku_idx ON products(sku)')
+        cursor2.execute('CREATE INDEX IF NOT EXISTS company_sku_idx ON company(sku)')
 
-    current_sku = None
-    previous_qty = None
-    qty_changes = []
+        cursor.execute('SELECT p.sku, p.qty '
+                       'FROM products p '
+                       'ORDER BY p.sku, p.created_at')
 
-    for row in rows:
-        sku, qty = row
+        sku_cache = {}
+        current_sku = None
+        previous_qty = None
+        qty_changes = []
 
-        if qty != previous_qty:
-            if current_sku is None:
-                current_sku = sku
-            if sku != current_sku:
-                sku_cache[current_sku] = qty_changes.copy()
-                current_sku = sku
-                qty_changes = []
-            qty_changes.append(qty)
-            previous_qty = qty
+        while True:
+            rows = cursor.fetchmany(1000)
+            if not rows:
+                break
+            for row in rows:
+                sku, qty = row
 
-    if current_sku is not None:
-        sku_cache[current_sku] = qty_changes.copy()
+                if qty != previous_qty:
+                    if current_sku is None:
+                        current_sku = sku
+                    if sku != current_sku:
+                        sku_cache[current_sku] = qty_changes
+                        current_sku = sku
+                        qty_changes = []
+                    qty_changes.append(qty)
+                    previous_qty = qty
 
-    sorted_sku_cache = dict(sorted(sku_cache.items(), key=lambda item: len(item[1]), reverse=True))
+        if current_sku is not None:
+            sku_cache[current_sku] = qty_changes
 
-    with open("txt/sorted.txt", "w") as file:
-        for sku, changes in sorted_sku_cache.items():
-            file.write(f'{sku}: {" - ".join(map(str, changes))}\n')
+        sorted_sku_cache = dict(sorted(sku_cache.items(), key=lambda item: len(item[1]), reverse=True))
 
+        with open("txt/sorted.txt", "w", encoding="utf-8") as file:
+            for sku, changes in sorted_sku_cache.items():
+                cursor2.execute("SELECT company_name, value FROM company WHERE sku=?", (sku,))
+                result = cursor2.fetchone()
+
+                if result is not None:
+                    company, value = result
+                    file.write(f'SKU: {sku}, Company Name: {company}, Value: {value}, Changes: {" - ".join(map(str, changes))}\n')
+                else:
+                    file.write(f'SKU: {sku}, Company Name: Data Not Found, Value: Data Not Found, Changes: {" - ".join(map(str, changes))}\n')
 
 def find_stable_products():
-    with open("txt/sorted.txt", "r") as file:
-        unchanged_products = [line.split(": ")[0] for line in file if " - " not in line]
-
+    with open("txt/sorted.txt", "r", encoding="utf-8") as file:
+        unchanged_products = [line.split("Changes: ")[0] for line in file if " - " not in line]
     created_at_dict = {}
     with sqlite3.connect('data.db') as conn:
         cursor = conn.cursor()
         cursor.execute("CREATE INDEX IF NOT EXISTS sku_index ON products (sku);")
 
         for product in unchanged_products:
-            cursor.execute("SELECT created_at FROM products WHERE sku=?", (product,))
-            created_at = cursor.fetchall()
+            parts = product.split(", ")
+            sku = parts[0].split(": ")[1]
+            company_name = parts[1].split(": ")[1]
+            value = parts[2].split(": ")[1]
+
+            cursor.execute("SELECT created_at FROM products WHERE sku=?", (sku,))
+            created_at = cursor.fetchone()
+
             if created_at:
-                created_at_dict[product] = created_at[0][0]
-            else:
-                created_at_dict[product] = "N/A"
+                created_at_dict[sku] = (created_at[0], company_name, value)
 
-    with open("txt/unchanged_products.txt", "w") as file:
-        for product, created_at in created_at_dict.items():
-            file.write(f"SKU: {product} - Date: {created_at}\n")
+    with open("txt/unchanged_products.txt", "w", encoding="utf-8") as file:
+        for sku, (created_at, company_name, value) in created_at_dict.items():
+            file.write(f"SKU: {sku}, Date: {created_at}, Company Name: {company_name}, Value: {value}\n")
+def calculate_stock_change(data):
+    parts = data.split(", ")
+    product_code = parts[0].split(": ")[1]
+    company_name = parts[1].split(": ")[1]
+    product_value = parts[2].split(": ")[1]
 
-def calculate_total_sales(data):
-    parts = data.split(": ")
-    product_code = parts[0]
-    sales = list(map(int, parts[1].split(" - ")))
-    total_sales = max(sales) - min(sales)
-    return f"{product_code}   Total Sales: {total_sales}\n"
+    sales = list(map(int, parts[-1].split(": ")[1].split(" - ")))
+    stock_change = 0
+
+    for i in range(len(sales)-1):
+        if sales[i] > sales[i+1]:
+            decrease = sales[i] - sales[i+1]
+            stock_change += decrease
+
+    return f"SKU: {product_code} Company Name: {company_name} Value: {product_value} Stock Change: {stock_change}\n"
 
 def process_sorted_file():
     input_file_path = 'txt/sorted.txt'
     output_file_path = 'txt/totalSales.txt'
 
     def check_stock_decrease(file_path):
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             lines = file.readlines()
             results = []
 
             for line in lines:
-                results.append(calculate_total_sales(line.strip()))
+                results.append(calculate_stock_change(line.strip()))
 
             return results
 
     results = check_stock_decrease(input_file_path)
 
     if results:
-        with open(output_file_path, 'w') as file:
+        with open(output_file_path, 'w',encoding="utf-8") as file:
             file.writelines(results)
     else:
-        with open(output_file_path, 'w') as file:
+        with open(output_file_path, 'w',encoding="utf-8") as file:
             pass
-
 
 if __name__ == "__main__":
     try:
@@ -137,7 +179,9 @@ if __name__ == "__main__":
         write_parsed_data_to_file(parsed_data)
         print("Parsed Data file creation operation completed.")
         write_to_database(parsed_data)
-        print("Database writing operation completed.")
+        print("Products Database writing operation completed.")
+        add_data_to_database()
+        print("Company Database writing operation completed.")
         generate_stock_changes()
         print("Sorted file creation operation completed.")
         process_sorted_file()
